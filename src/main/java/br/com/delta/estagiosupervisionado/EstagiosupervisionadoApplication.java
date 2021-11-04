292096c55cf7f82b396bc42eb7eecd040d78e240
Lucas G. Silva <gitportifoliolucas@gmail.com>
2021-11-03 21:15:14 -0300

    Projeto criado

---
+++ src/main/java/br/com/delta/estagiosupervisionado/EstagiosupervisionadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.delta.estagiosupervisionado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstagiosupervisionadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstagiosupervisionadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/delta/estagiosupervisionado/EstagiosupervisionadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delta.estagiosupervisionado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstagiosupervisionadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
