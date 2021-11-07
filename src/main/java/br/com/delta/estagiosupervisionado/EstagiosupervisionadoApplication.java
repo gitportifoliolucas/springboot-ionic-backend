8c523a471bf09cc4fc2c356b1b60ebc46a7dcfc1
Lucas G. Silva <gitportifoliolucas@gmail.com>
2021-11-07 10:40:01 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/delta/estagiosupervisionado/EstagiosupervisionadoApplication.java
+++ src/main/java/br/com/delta/estagiosupervisionado/EstagiosupervisionadoApplication.java
@@ -1,13 +1,32 @@
 package br.com.delta.estagiosupervisionado;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.delta.estagiosupervisionado.domain.Categoria;
+import br.com.delta.estagiosupervisionado.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class EstagiosupervisionadoApplication {
+public class EstagiosupervisionadoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstagiosupervisionadoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
