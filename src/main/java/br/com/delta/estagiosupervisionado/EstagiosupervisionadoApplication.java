f511910bb347eff1658f9010971a8b9bd6f2ea8d
Lucas G. Silva <gitportifoliolucas@gmail.com>
2021-11-13 22:12:23 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/delta/estagiosupervisionado/EstagiosupervisionadoApplication.java
+++ src/main/java/br/com/delta/estagiosupervisionado/EstagiosupervisionadoApplication.java
@@ -13,6 +13,7 @@ import br.com.delta.estagiosupervisionado.domain.Cidade;
 import br.com.delta.estagiosupervisionado.domain.Cliente;
 import br.com.delta.estagiosupervisionado.domain.Endereco;
 import br.com.delta.estagiosupervisionado.domain.Estado;
+import br.com.delta.estagiosupervisionado.domain.ItemPedido;
 import br.com.delta.estagiosupervisionado.domain.Pagamento;
 import br.com.delta.estagiosupervisionado.domain.PagamentoComBoleto;
 import br.com.delta.estagiosupervisionado.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import br.com.delta.estagiosupervisionado.repositories.CidadeRepository;
 import br.com.delta.estagiosupervisionado.repositories.ClienteRepository;
 import br.com.delta.estagiosupervisionado.repositories.EnderecoRepository;
 import br.com.delta.estagiosupervisionado.repositories.EstadoRepository;
+import br.com.delta.estagiosupervisionado.repositories.ItemPedidoRepository;
 import br.com.delta.estagiosupervisionado.repositories.PagamentoRepository;
 import br.com.delta.estagiosupervisionado.repositories.PedidoRepository;
 import br.com.delta.estagiosupervisionado.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class EstagiosupervisionadoApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstagiosupervisionadoApplication.class, args);
@@ -114,6 +118,19 @@ public class EstagiosupervisionadoApplication implements CommandLineRunner {
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 
