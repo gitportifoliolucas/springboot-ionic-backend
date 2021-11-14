93ebf4da83e303f5dfb9df4c76b0aec91517e018
Lucas G. Silva <gitportifoliolucas@gmail.com>
2021-11-13 22:54:25 -0300

    Endpoint / pedidos/{id} disponibilizado

--- src/main/java/br/com/delta/estagiosupervisionado/domain/PagamentoComBoleto.java
+++ src/main/java/br/com/delta/estagiosupervisionado/domain/PagamentoComBoleto.java
@@ -4,13 +4,18 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import br.com.delta.estagiosupervisionado.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
-	
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataPagamento;
 
 	public PagamentoComBoleto() {
@@ -40,5 +45,5 @@ public class PagamentoComBoleto extends Pagamento {
 	public void setDataPagamento(Date dataPagamento) {
 		this.dataPagamento = dataPagamento;
 	}
-	
+
 }
